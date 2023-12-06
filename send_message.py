6ee997bdbc0f8d3277a88db1fdd2f99710260512
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-06 10:19:19 +0500

    button

--- send_message.py
+++ send_message.py
@@ -13,14 +13,22 @@ def send_message(chat_id, text, reply_markup):
     requests.post(url, json=payload)
 
 btn1 = {'text': 'button 1'}
-btn2 = {'text': 'button 2'}
-btn3 = {'text': 'button 3'}
+btn2 = {
+    'text': 'share location',
+    'request_location': True
+}
+btn3 = {
+    'text': 'share contact',
+    'request_contact': True
+}
 
 reply_markup = {
     "keyboard": [
         [btn1, btn2],
         [btn3]
-    ]
+    ],
+    "one_time_keyboard": True,
+    "resize_keyboard": True
 }
 
 send_message(chat_id=chat_id, text='ok', reply_markup=reply_markup)
