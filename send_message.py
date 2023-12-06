d0ed5ae7149e7ad33c615688d14e1cdb0f9d9a8e
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-06 10:13:04 +0500

    keyboards

--- send_message.py
+++ send_message.py
@@ -1,14 +1,26 @@
-from settings import TOKEN
+from settings import TOKEN, chat_id
 import requests
 
 
 
-def send_message(chat_id, text):
+def send_message(chat_id, text, reply_markup):
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
     payload = {
         "chat_id": chat_id,
         "text": text,
+        'reply_markup': reply_markup
     }
-    requests.get(url, params=payload)
+    requests.post(url, json=payload)
 
-# send_message('1258594598', 'hi')
+btn1 = {'text': 'button 1'}
+btn2 = {'text': 'button 2'}
+btn3 = {'text': 'button 3'}
+
+reply_markup = {
+    "keyboard": [
+        [btn1, btn2],
+        [btn3]
+    ]
+}
+
+send_message(chat_id=chat_id, text='ok', reply_markup=reply_markup)
